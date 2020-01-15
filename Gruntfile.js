3fdf62c34780ba0f516c48b5089332f7e8ee9e32
Umherirrender <umherirrender_de.wp@web.de>
2020-01-15 19:45:00 +0100

    build: Add vendor to exclude list for eslint and jsonlint
    This avoids failures on vendor files
    
    Change-Id: If6c00edcfa048b139d2afa8503e9b076c18facf0

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,7 +15,8 @@ module.exports = function ( grunt ) {
 			},
 			all: [
 				'**/*.js',
-				'!node_modules/**'
+				'!node_modules/**',
+				'!vendor/**'
 			]
 		},
 		stylelint: {
@@ -26,7 +27,8 @@ module.exports = function ( grunt ) {
 		jsonlint: {
 			all: [
 				'**/*.json',
-				'!node_modules/**'
+				'!node_modules/**',
+				'!vendor/**'
 			]
 		},
 		banana: conf.MessagesDirs,
